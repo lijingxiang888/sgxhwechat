67e62d5caac698be82760cff3ed910669e264af9
lijingxiang888 <924415666@qq.com>
2018-07-23 17:59:37 +0800

    添加跳转栏目

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,24 @@
 const app = getApp()
 
 Page({
-  data: {
-    imgs:[
-      '/images/banner1.jpg',
-      '/images/banner2.jpg',
-      '/images/banner3.jpg',
-      '/images/banner4.jpg'
-    ]
-  },
-  tel:function() {
-    wx.makePhoneCall({
-      phoneNumber: '18233283758',
-      success:function(){
-        console.log('拨打电话成功')
-      },
-      fail:function(){
-        console.log('拨打电话失败')
-      }
-    })
-  }
+    data: {
+        imgs: [
+            '/images/banner1.jpg',
+            '/images/banner2.jpg',
+            '/images/banner3.jpg',
+            '/images/banner4.jpg'
+        ]
+    },
+    tel: function() {
+        wx.makePhoneCall({
+            phoneNumber: '18233283758',
+            success: function() {
+                console.log('拨打电话成功')
+            },
+            fail: function() {
+                console.log('拨打电话失败')
+            }
+        })
+    }
 
 })
